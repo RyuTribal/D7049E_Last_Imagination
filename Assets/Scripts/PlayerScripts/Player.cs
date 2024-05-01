0c5aa9146284321e5076ba4cd2b6b56383190d3d
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2024-05-02 01:04:49 +0200

    Added a way to controll the player

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -17,10 +17,26 @@ namespace Last_Imagination
 
         [HVEEditableField]
         private float Max_Speed = 100.0f;
+
+        [HVEEditableField]
+        private float CameraSensativity = 1.0f;
+
+
+        private CameraComponent m_Camera;
+
+        private Vector2 m_CursorLastPos;
+        private Vector2 m_DeltaPos = new Vector2(0.0f);
+
+        [HVEEditableField]
+        private bool Inverse = false;
+
         void OnCreate()
         {
             m_BoxCollider = GetComponent<BoxColliderComponent>();
             Console.WriteLine($"Player {ID} created");
+
+            m_Camera = GetComponent<CameraComponent>();
+            m_CursorLastPos = GetMousePosition();
         }
 
         void OnUpdate(float delta_time)
@@ -30,17 +46,45 @@ namespace Last_Imagination
             if(m_BoxCollider != null) 
             {
                 Vector3 velocity = new Vector3(0.0f);
+                Vector3 forward_vector = m_Camera.GetForwardDirection();
+                Vector3 right_vector = m_Camera.GetRightDirection();
                 if (IsKeyPressed(KeyCode.D))
                 {
-                    velocity.X += Speed;
+                    Vector3 right = right_vector * Speed;
+                    velocity += right;
                 }
 
                 if(IsKeyPressed(KeyCode.A)) 
                 {
-                    velocity.X += -Speed;
+                    Vector3 left = right_vector * -Speed;
+                    velocity += left;
+                }
+
+                if (IsKeyPressed(KeyCode.W))
+                {
+                    Vector3 forward = forward_vector * Speed;
+                    velocity += forward;
+                }
+
+                if (IsKeyPressed(KeyCode.S))
+                {
+                    Vector3 back = forward_vector * -Speed;
+                    velocity += back;
                 }
 
                 m_BoxCollider.AddLinearVelocity(velocity * delta_time);
+            }
+
+
+            Vector2 m_CurrentPos = GetMousePosition();
+
+            m_DeltaPos = m_CurrentPos - m_CursorLastPos;
+
+            m_CursorLastPos = m_CurrentPos;
+
+            if (m_Camera != null)
+            {
+                m_Camera.RotateAroundEntity(m_DeltaPos, CameraSensativity * delta_time, Inverse);
             }
         }
     }
