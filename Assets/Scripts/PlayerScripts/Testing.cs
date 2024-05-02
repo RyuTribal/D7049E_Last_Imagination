efcd0f418c499c0bfd0d7d060e71772deb8429dc
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2024-05-02 23:56:52 +0200

    Added some stuff

---
+++ Assets/Scripts/PlayerScripts/Testing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Helios;
+
+namespace Last_Imagination
+{
+    public class Testing : Entity
+    {
+        private BoxColliderComponent m_Coller;
+
+        public void OnCreate()
+        {
+            m_Coller = GetComponent<BoxColliderComponent>();
+        }
+        public void OnUpdate(float dt)
+        {
+            if (m_Coller != null)
+            {
+                if (IsKeyPressed(KeyCode.D))
+                {
+                    Vector3 velocity = new Vector3(0.0f);
+                    velocity.X = 10.0f * dt;
+                    m_Coller.AddLinearVelocity(velocity);
+                }
+                
+            }
+
+            
+        }
+    }
+}
